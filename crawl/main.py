35a6e27afadf7995b1aae8d3d337c0ee7e77d16f
addbook <oskcloud@gmail.com>
2014-09-08 13:22:08 +0800

    Create Flask app utils etc.

---
+++ crawl/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+# vim: set et sw=4 ts=4 sts=4 fenc=utf-8
+# Author: YuanLin
+
+import os
+from flask_script import Manager, Server
+from crawl.app import setup
